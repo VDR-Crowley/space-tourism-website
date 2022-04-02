87deee9967bd30226205fb497052a3e8212ec231
Vando Dos Reis <vandodosreis2001@gmail.com>
2022-04-02 13:37:17 -0300

    build melhoria do header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
 
   React.useEffect(() => {
     setActiveItem(location.pathname);
-  }, []);
+  }, [location.pathname]);
 
   return (
     <header className="w-full flex mt-5 items-center justify-between max-w-screen-2xl mx-auto">
